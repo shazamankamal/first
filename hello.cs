882f9dc0b07611a54ac3ef1e1d7ef2d4615bc27d
shazamankamal <shazaman.kamal@xtreamit.com>
2017-07-25 19:26:03 +0530

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -11,6 +11,7 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("HELLO");
+             Console.WriteLine("shaz");
             Console.ReadLine();
         }
     }
